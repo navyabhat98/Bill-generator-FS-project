057c58df58365e7f7d8f8fd46c83cbbeaa2e3943
navyabhat98 <blnavya@gmail.com>
2019-05-09 00:45:45 +0530

    Third commit

--- billing.cpp
+++ billing.cpp
@@ -10,24 +10,25 @@ using namespace std;
 class items
 {
 	//items
-	string itemnames[100];
-	string itemvalues[100];
+	string itemnames;
+	string itemvalues;
 	string monthname;
 	string buffer;
-	double price[100];
+	float amount;
+	float price;
 	int qty;
+
 	public:
 		//fucntions to pack and unpack item names into a file, storeitems.txt
 		void store_item_names();
-		void unpack_item_names();
+		void unpack_item_names(int);
 
 		//fucntion to append more items
 		void append_itemname();
 
 		//fucntion to store monthly income and generate a bill and store it in a file.
 		//the bill will be stored as monthname-year.txt
-		void insert_monthly_income();
-		void generate_bill();
+		void insert_monthly_income(string);
 		void store_bill(string);
 		void display_bill(string);
 };
@@ -39,7 +40,7 @@ void items::append_itemname()
 	cout<<"\nEnter item to be appended to file:";
 	getline(cin, buffer);
 	cout<<"\nEnter Price of item:";
-	//cin>>price;
+	cin>>price;
 	//store it in a file
 	store_item_names();
 }
@@ -52,34 +53,35 @@ void items::store_item_names()
 	file.close();
 }
 
-void items::unpack_item_names()
+void items::unpack_item_names(int i)
 {
-	int i=0,j=0;
+	while(buffer[i]!='|')
+		itemnames += buffer[i++];
+	while(buffer[i]!='$');
+		//get the price value
+}
+void items::insert_monthly_income(string month)
+{
+	int i;
 	fstream file;
 	file.open("storeitems.txt",ios::in);
 	while(!file.eof())
 	{
+		i = 0;
 		buffer.erase();
 		getline(file,buffer);
-		while(buffer[i]!='|')
-			itemnames[j] += buffer[i++];
-		while(buffer[i]!='$')
+		unpack_item_names(i);
+		
+		cout<<"\n"<<itemnames<<":";	cin>>itemvalues;
+		cout<<"\nQuantity:";		cin>>qty;
+		amount = price*qty;
+		store_bill(month);
 			
-		j++;
+		
 	}
-	cout<<itemnames[0]<<"\n"<<itemnames[1];
-	file.close();
-
-}
-void items::insert_monthly_income()
-{
-	//read the items
+	file.close();	
 }
 
-void items::generate_bill()
-{
-	//bill with proper format is generated
-}
 
 void items::store_bill(string month)
 {
@@ -88,14 +90,15 @@ void items::store_bill(string month)
 	monthname+=".txt";
 	fstream file;
 	file.open(monthname,ios::out|ios::app);
-	file<<buffer;
+	file<<itemnames<<"\t"<<qty<<"\t"<<amount<<"$"<<"\n";
 	file.close();
 }
 
 void items::display_bill(string date)
 {
 	date += ".txt";
-	system("cat.txt");
+	string s = "cat "+date;
+	//system(s);
 }	
 
 
@@ -103,7 +106,33 @@ int main()
 {
 
 	items i;
-	i.unpack_item_names();
+	int choice;
+	string month;
+	while(1) 
+	{
+		cout<<"\nMENU\n";
+		cout<<"1. Monthly income updatation\n";
+		cout<<"2. Display bill\n";
+		cout<<"3. Entering new Item in store.\n";
+		cout<<"4.Exit\n";
+		cout<<"Enter choice:";	cin>>choice;
+		switch(choice)
+		{
+			case 1: system("clear");
+				cout<<"\nEnter month name and year(monthnameyy):"; cin>>month;	
+				i.insert_monthly_income(month);
+				break;
+			case 2: system("clear");
+				cout<<"\nEnter month name and year(monthnameyy):";	cin>>month;
+				i.display_bill(month);
+				break;
+			case 3: system("clear");
+				cout<<"\nNew Item to store list\n";
+				i.append_itemname();
+			case 4: exit(0);
+			default: cout<<"Invalid choice\n";
+		}
+	}
 	
 	return 0;
 }
